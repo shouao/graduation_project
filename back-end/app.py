f8041a614462e35b9bae9e0d632f4fca84ce7fa4
shouao <792417939@qq.com>
2023-02-25 17:12:26 +0800

    dummy backend

--- back-end/app.py
+++ back-end/app.py
@@ -1,25 +1,25 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/api/predict', methods=['POST'])
 def test():
     data = request.get_json()
-    repo = data['repo']
+    repoUrl = data['repoUrl']
     algorithm = data['algorithm']
 
     # 这里可以根据 repo 和 algorithm 做一些处理，生成测试结果 response
 
     response = {
         'success': True,
-        'data': {
-            'files': [
-                'test_file_1.txt',
-                'test_file_2.txt'
-            ],
-            'accuracy': 0.6,
-            'f1-score': 0.8
-        }
+        'files': [
+            'test_file_1.txt',
+            'test_file_2.txt'
+        ],
+        'accuracy': 0.6,
+        'f1_score': 0.8
     }
 
     return jsonify(response)
--- front-end/re_deploy.sh
+++ front-end/re_deploy.sh
@@ -3,4 +3,4 @@ docker rm $(docker ps -a -q --filter ancestor=front-end --format="{{.ID}}")
 docker rmi front-end
 
 docker build -t front-end .
-docker run -d -p 80:80 -p 5000:5000 front-end
+docker run -d --network host front-end
